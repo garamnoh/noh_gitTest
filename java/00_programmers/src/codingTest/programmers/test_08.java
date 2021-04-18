1e01f5e35a9a56d08a558d89caed5c1520ad8a07
garam noh <garam.nohh@gmail.com>
2021-04-18 15:07:45 +0900

    no message

---
+++ java/00_programmers/src/codingTest/programmers/test_08.java
@@ -0,0 +1,44 @@
+package codingTest.programmers;
+
+public class test_08 {
+	
+	public static void main(String[] args) {
+		
+//		2016년 1월 1일은 금요일입니다.
+//		2016년 a월 b일은 무슨 요일일까요? 
+//		두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요.
+//		요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT입니다.
+//		예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
+//
+//		제한 조건
+//		2016년은 윤년입니다.
+//		2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+//		
+//		입출력 예
+//		a	b	result
+//		5	24	"TUE"
+		
+		int a = 5;
+		int b = 24;
+		
+        String answer = "";
+        
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        for(int i = 1; i < a; i++) {
+        	b += month[i-1];
+        }
+        
+        switch(b%7) {
+        	case 0: answer = "THU"; break;
+        	case 1: answer = "FRI"; break;
+        	case 2: answer = "SAT"; break;
+        	case 3: answer = "SUN"; break;
+        	case 4: answer = "MON"; break;
+        	case 5: answer = "TUE"; break;
+        	case 6: answer = "WED"; break;
+        }
+        
+        System.out.println(answer);	
+	}
+}
---
+++ python/programmers/2016년.py
@@ -0,0 +1,34 @@
+# 2016년 1월 1일은 금요일입니다.
+# 2016년 a월 b일은 무슨 요일일까요? 
+# 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요.
+# 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT입니다.
+# 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
+
+# 제한 조건
+# 2016년은 윤년입니다.
+# 2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+
+# 입출력 예
+# a	b	result
+# 5	24	"TUE"
+
+answer = ''
+
+a = 5
+b = 24
+
+month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+if a > 1:
+  for i in month[0:a-1]:
+    b += i
+
+if b%7 == 0 : answer = 'THU'
+elif b%7 == 1 : answer = 'FRI'
+elif b%7 == 2 : answer = 'SAT'
+elif b%7 == 3 : answer = 'SUN'
+elif b%7 == 4 : answer = 'MON'
+elif b%7 == 5 : answer = 'TUE'
+else: answer = 'WED'
+
+print(answer)
